a84043b29c594f98374b38ab623078de0c1b0a6b
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2023-01-09 01:46:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-import os
-import sys
+import getpass
 import github
 
-YOUR_ACCESS_TOKEN = input("Please enter your github access token: ") 
-client = github.GitHub(access_token=YOUR_ACCESS_TOKEN)
+# Prompt the user for their GitHub username and access token
+username = input("Enter your GitHub username: ")
+access_token = getpass.getpass("Enter your GitHub access token: ")
 
+# Create a GitHub API client using the provided username and access token
+client = github.Github(login_or_token=access_token, user=username)
 
-repos = client.repos.list()
+# Next, get the list of repositories for the authenticated user
+repos = client.get_user().get_repos()
 
+# Print the names of the repositories
 print("Repositories:")
 for i, repo in enumerate(repos):
     print(f"{i+1}: {repo.name}")
 
+# Prompt the user to select one or more repositories to delete
 selected_indices = []
 while True:
-    selected_index = input("Enter the number of a repository you want to delete (press enter once you are done): ")
-    if selected_index == "":
+    choice = input("Enter the number of a repository you want to delete (or press enter to finish): ")
+    if choice == "":
         break
-    selected_indices.append(int(selected_index))
+    selected_indices.append(int(choice))
 
+# Delete the selected repositories
 for i in selected_indices:
     selected_repo = repos[i-1]
-    client.repos.delete(selected_repo.owner.login, selected_repo.name)
+    selected_repo.delete()
     print(f"Successfully deleted repository {selected_repo.name}")
